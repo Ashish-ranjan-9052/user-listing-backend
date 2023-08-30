efa5dcdd3476e22f3797f3c3c80355b3934cc1aa
Ashish-ranjan-9052 <ashish.ranjan@nimblework.com>
2023-08-30 10:22:16 +0530

    first

---
+++ src/main/java/com/example/Userlisting/UserlistingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Userlisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserlistingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserlistingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Userlisting/UserlistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Userlisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserlistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
